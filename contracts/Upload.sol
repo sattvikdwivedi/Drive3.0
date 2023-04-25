384f30feadf3191724c835f02455eb97dd3b355e
sattvikdwivedi <sattvikdwivedi2001@gmail.com>
2023-04-25 20:35:38 +0530

    Initial commit

---
+++ contracts/Upload.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+  
+  contract Upload{
+    struct Access{
+          address user;
+          bool access;
+    }
+    mapping(address=>string[]) value;
+    mapping(address=>Access[])accessList;
+    mapping(address=>mapping(address=>bool))ownership;
+    mapping(address=>mapping(address=>bool))previousData;
+    
+    function add(address user,string memory url)public
+{
+     value[user].push(url);
+}
+
+function allow(address user) public{
+   ownership[msg.sender][user]=true;
+   if(previousData[msg.sender][user]==true){
+     for(uint i=0;i<accessList[msg.sender].length;i++){
+       if( accessList[msg.sender][i].user==user){
+          accessList[msg.sender].push(Access(user,true));}}}
+     else{
+
+       accessList[msg.sender].push(Access(user,true));
+       previousData[msg.sender][user]=true;
+     }
+   }
+   function disallow(address user) public{
+    ownership[msg.sender][user]=false;
+    for(uint i=0;i<accessList[msg.sender].length;i++){
+    if( accessList[msg.sender][i].user==user){
+     accessList[msg.sender][i].access=false; 
+     
+    }
+    }
+   }
+   function display(address _user) external view returns( string[] memory){
+   require( msg.sender==_user || ownership[_user][msg.sender],"You are not allowed");
+   return value[_user];
+
+   }
+   function shareAccess()  public view returns  (Access[] memory){
+     return accessList[msg.sender];
+   }
+
+}
+
+
+      
+  
+  
+
+
